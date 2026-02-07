894ceeeb503ff177698863bc8d9d2402e55f655a
PeterCDMcLean <peter.cd.mclean@gmail.com>
2026-02-07 08:05:35 -0500

    Update jellyfin-sdk-kotlin dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,7 +136,7 @@ dependencies {
 
     // Network
     val sdkVersion = findProperty("sdk.version")?.toString()
-	implementation("com.github.PeterCDMcLean:jellyfin-sdk-kotlin:release-1.6.z-SNAPSHOT")
+    implementation("com.github.PeterCDMcLean:jellyfin-sdk-kotlin:jellyfin-core-android-release:release-1.6.z-a5327fd936-1")
     implementation(libs.okhttp)
     implementation(libs.okio)
     implementation(libs.coil)
